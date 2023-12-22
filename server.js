5694cba7be0d6296413b5cc591b7c19b254cc2a8
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-12-22 19:08:42 +0200

    mangoose added

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const app = require("./app");
+const express = require("express");
+// const cors = require("cors");
+const mongoose = require("mongoose");
+// require("dotenv").config();
+const app = express();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = `mongodb+srv://ovchrk:Nastya03082017@cluster0.qaqm6vd.mongodb.net/db-contacts?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
