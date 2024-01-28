f305d9d0a0c9d1dec244a1df6f2968dd3290b798
ovchrk <113932934+ovchrk@users.noreply.github.com>
2024-01-28 17:28:27 +0200

    token_fixed

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -52,9 +52,10 @@ const verifyEmail = async (req, res, next) => {
   if (!user) {
     throw HttpError(404, "User not found");
   }
+
   await User.findByIdAndUpdate(user._id, {
     verify: true,
-    verificationCode: "",
+    verificationToken: null,
   });
 
   res.status(200).json({ message: "Verification successful" });
